7f2569cf4dbfd53f68a9a8b1a52f6eeeb5ac493a
truelala58 <55549323+truelala58@users.noreply.github.com>
2021-11-26 21:49:56 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/autoTests/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/autoTests/sandbox/MyFirstProgram.java
@@ -1,9 +1,28 @@
 package ru.stqa.autoTests.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram{
 
   public static void main(String[] args){
-    System.out.println("hello, world!");
+    hello("world");
+    hello("you");
+    double l =5;
+    System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
+    double a=4;
+    double b=6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+
+  }
+
+  public static void hello (String sombody) {
+    System.out.println("hello, "+ sombody + "!");
+  }
+  public static double area (double len){
+    return  len*len;
+  }
+  public  static double area (double a, double b) {
+    return a*b;
   }
 
 }
