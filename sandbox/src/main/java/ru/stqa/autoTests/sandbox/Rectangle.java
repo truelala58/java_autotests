18d1d1be83b4fa66df6c0ecc2d131ae1c00fb2dc
truelala58 <55549323+truelala58@users.noreply.github.com>
2021-12-03 21:51:57 +0300

    Добавлен тестовый класс

--- sandbox/src/main/java/ru/stqa/autoTests/sandbox/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/autoTests/sandbox/Rectangle.java
@@ -6,7 +6,7 @@ public class Rectangle {
     public Rectangle(double a, double b){
         this.a=a;
         this.b=b;
-    };
+    }
     public double area() {
         return this.a * this.b;
     }
